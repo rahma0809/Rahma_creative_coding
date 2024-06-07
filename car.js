afd6e9ae2adbdf222c4b460a3ad93a13989afe0d
rahma0809 <144330496+rahma0809@users.noreply.github.com>
2024-06-07 16:05:22 +0400

    Create car.js

---
+++ car.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+  
+  fill(150,0,90);
+  rect(100,200,210,50);
+  rect(130,170,150,50);
+  
+  fill(0,210,230);
+  rect(140,180,50,40);
+  rect(220,180,50,40);
+  
+  fill(0);
+  ellipse(130, 260,40,40);
+  ellipse(270, 260,40,40);
+  
+  fill(90,30,20)
+  ellipse(300, 220,20,20);
+}
+        
